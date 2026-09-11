04b74fe8034e4ad80fee3b0ffcd9105cb77bb681
Inventor-creator <aryaupatil9@gmail.com>
2026-09-11 15:04:24 +0530

    stuff

--- wzBackend/Routes/userRoutes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
-from database.models import User
-router = APIRouter(prefix="/users")
-
-@router.get("/")
-def get_users(db: Session = Depends()):
-    users = db.query(User).all()
-    return users
